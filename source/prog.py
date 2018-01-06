17ccfdecdfeb5585af474c1fcd671758794251cc
yang45001 <yang45001@aliyun.com>
2018-01-06 20:38:39 +0800

    保存文本成csv格式

--- source/prog.py
+++ source/prog.py
@@ -1,21 +1,75 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import argparse
+import csv
 
 import sys
 
+FILE='test.csv'
+savedata=[]
+datalen=0
+headers=['account','pwd']
+
 #function_switch={'i':insertdata(),'l':listdata(),'g':getdata(),'d':deldata()}
 def insertdata():
-    print '*****insertdata*****'
+    global datalen
+    name=raw_input("input you name:\n")
+    passwd=raw_input("input passwd:\n")
+    job={}
+    job['account']=name
+    job['pwd']=passwd
+    datalen=len(savedata)
+    for i in range(datalen+1):
+        if i>=datalen:
+            savedata.append(job)
+            writecsv()
+            break
+        row=savedata[i]
+        if row['account']==name:
+            print '*****userattricek******'
+            print 'frist del usr'
+            break
+    #if savedata.count(job)==0:
+    #    savedata.append(job)
+    #    writecsv()
+    #    print '*****insertdata*****'
+    #else:
+    #    print'*****insertfail*****'
+
 
 def listdata():
+    for row in savedata:
+        print row[headers[0]],row[headers[1]]
     print '*****listdata*****'
 
 def getdata():
-    print '*****listdata*****'
+    name=raw_input("input username\n")
+    global datalen
+    for i in range(datalen+1):
+        if i>=datalen:
+            print '*****no find user****'
+            break
+        row=savedata[i]
+        if row['account']==name:
+            print row['pwd']
+            print '*****getdata success*****'
+            break
 
 def deldata():
-    print '*****deldata*****'
+    global datalen
+    datalen=len(savedata)
+    name=raw_input("input name\n")
+#    print datalen
+    for i in range(datalen+1):
+        if i>=datalen:
+            print '*****delfail*****'
+            break
+        row=savedata[i]
+        if row['account']==name:
+            savedata.remove(row)
+            writecsv()
+            print'*****deldata*****'
+            break
 
 
 #yyfunction_switch={'i':insertdata(),'l':listdata(),'g':getdata(),'d':deldata()}
@@ -35,5 +89,23 @@ def init():
         print 'input error'
     # print parser.print_help()
 #function_switch={'i':insertdata(),'l':listdata(),'g':getdata(),'d':deldata(
+
+def writecsv():
+    with open(FILE,'w') as f:
+        writer=csv.DictWriter(f,headers)
+        writer.writeheader()
+        writer.writerows(savedata)
+    pass
+
+def readcsv():
+    global datalen
+    with open(FILE,'r') as f:
+        temp=csv.DictReader(f)
+        for row in temp:
+            savedata.append(row)
+    datalen=len(savedata)
+    pass
+
 if __name__ == '__main__':
+    readcsv()
     init()
